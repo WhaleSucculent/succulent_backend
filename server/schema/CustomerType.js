6e31bbf01e2fb72cbd04a3feb058af40d6bee5c2
bbek101-svg <84989535+bbek101-svg@users.noreply.github.com>
2022-06-14 15:56:37 -0600

    Customer type update

--- server/schema/CustomerType.js
+++ server/schema/CustomerType.js
@@ -1,38 +1,38 @@
 import {
-   GraphQLObjectType,
-   GraphQLID,
-   GraphQLString,
-   GraphQLInt,
-   GraphQLBoolean,
-   GraphQLScalarType,
- } from 'graphql';
- import dateScalar from './CustomScalar.js';
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLBoolean,
+  GraphQLScalarType,
+} from "graphql";
+import AddressType from "./AddressType.js";
+import dateScalar from "./CustomScalar.js";
 
-
- const CreditCardType = new GraphQLObjectType({
-   name: 'CreditCard',
-   fields: () => ({
-      cardNo: { type: GraphQLString },
-      holderName: { type: GraphQLString },
-      expirationDate: { type: dateScalar },
-   }),
+const CreditCardType = new GraphQLObjectType({
+  name: "CreditCard",
+  fields: () => ({
+    cardNo: { type: GraphQLString },
+    holderName: { type: GraphQLString },
+    expirationDate: { type: dateScalar },
+  }),
 });
 
- const CustomerType = new GraphQLObjectType({
-   name: 'Customer',
-   fields: () => ({
-      email:{ type: GraphQLString },
-      password:{ type: GraphQLString },
-      firstName:{ type: GraphQLString },
-      lastName:{ type: GraphQLString },
-      phone:{ type: GraphQLString },
-      status:{ type: GraphQLBoolean },
-      role:{ type: GraphQLString },
-      wechatId:{ type: GraphQLString },
-      paypalId:{ type: GraphQLString },
-      creditCards:{ type: CreditCardType },
-      addresses:{ type: new GraphQLList(AddressType) },
-   })
- });
+const CustomerType = new GraphQLObjectType({
+  name: "Customer",
+  fields: () => ({
+    email: { type: GraphQLString },
+    password: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    lastName: { type: GraphQLString },
+    phone: { type: GraphQLString },
+    status: { type: GraphQLBoolean },
+    role: { type: GraphQLString },
+    wechatId: { type: GraphQLString },
+    paypalId: { type: GraphQLString },
+    creditCards: { type: CreditCardType },
+    addresses: { type: AddressType },
+  }),
+});
 
- export default CustomerType;
+export default CustomerType;
