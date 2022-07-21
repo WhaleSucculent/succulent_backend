81e58bb526bd380f08bc82fe7404b9dfea7db1e3
flowersmiling <lcl8885@gmail.com>
2022-07-21 17:51:58 -0600

    update payment

--- controllers/payment-api.js
+++ controllers/payment-api.js
@@ -3,38 +3,44 @@ import Stripe from 'stripe';
 const stripe = Stripe('sk_test_51LKTrMA5EeipctR93rEE3ADPuXTkoF6okOrYuD7WosQaOorJGJHTlaaNryr9k62wXPh1O8lsW4jAowAmHPnH42Yt006EQbnHJa');
 
 const newPay = async (req, res) => {
-    const { totalAmount, cancelRoute, productIds, currentRoute } = req.body;
-    try {
-      // Create Checkout Sessions from body params.
-      const session = await stripe.checkout.sessions.create({
-        payment_method_types: ["card"],
-        line_items: [
-          {
-            price_data: {
-              currency: "usd",
-              product_data: {
-                name: "Whale Succulent Supply",
-                images: ["https://i.imgur.com/8DYoXXs.jpeg"],
-              },
-              unit_amount: parseFloat(totalAmount, 10) * 100,
+  res.header("Access-Control-Allow-Origin: *");
+  res.header("Access-Control-Allow-Credentials: true ");
+  res.header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
+  res.header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
+  
+  const { totalAmount, cancelRoute, productIds, currentRoute } = req.body;
+  
+  try {
+    // Create Checkout Sessions from body params.
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: "Whale Succulent Supply",
+              images: ["https://i.imgur.com/8DYoXXs.jpeg"],
             },
-            quantity: 1,
+            unit_amount: parseFloat(totalAmount, 10) * 100,
           },
-        ],
-        mode: "payment",
-        // success_url: `${
-        //   req.headers.origin
-        // }?is_stripe=true&is_cart=${currentRoute.includes(
-        //   "cart"
-        // )}&product_ids=${productIds}`,
-        success_url: `https://succulent-frontend.vercel.app${currentRoute}?is_stripe=true`,
-        cancel_url: `${req.headers.origin}${cancelRoute}`,
-      });
+          quantity: 1,
+        },
+      ],
+      mode: "payment",
+      // success_url: `${
+      //   req.headers.origin
+      // }?is_stripe=true&is_cart=${currentRoute.includes(
+      //   "cart"
+      // )}&product_ids=${productIds}`,
+      success_url: `https://succulent-frontend.vercel.app${currentRoute}?is_stripe=true`,
+      cancel_url: `${req.headers.origin}${cancelRoute}`,
+    });
 
-      res.json({ url: session.url });
-    } catch (err) {
-      res.status(500).json({ statusCode: 500, message: err.message });
-    }
+    res.json({ url: session.url });
+  } catch (err) {
+    res.status(500).json({ statusCode: 500, message: err.message });
+  }
 };
 
 export default newPay;
