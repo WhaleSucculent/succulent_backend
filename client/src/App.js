da81d79c040e702b53451ab7c7b4e5da3a76f269
Jiasheng Lu <1473875731@qq.com>
2022-07-21 23:15:33 -0600

    fix: proxy and deploy bugs

--- client/src/App.js
+++ client/src/App.js
@@ -50,7 +50,7 @@ const cache = new InMemoryCache({
 });
 
 const client = new ApolloClient({
-  uri: "https://succulentbackend.azurewebsites.net/graphql",
+  uri: 'http://localhost:5000/graphql',
   cache,
 });
 
