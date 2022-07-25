5db1f04a5f35d6b5a97109e3a5589fad20e387e7
Jiasheng Lu <1473875731@qq.com>
2022-07-25 12:51:48 -0600

    add check for development or dev  env

--- server/index.js
+++ server/index.js
@@ -86,6 +86,23 @@ app.use(
 app.use(urlencoded({extended:false}))
 app.use(json())
 
+
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/client/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
+}
+
 app.use('/', routes);
+
 
 app.listen(port, console.log(`Server running on port ${port}`));
